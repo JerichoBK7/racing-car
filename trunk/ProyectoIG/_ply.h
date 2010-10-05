b405d950b70e48f67773d69baf199361f6f89c58
esloguin@gmail.com <esloguin@gmail.com@3f51877b-c059-160e-7b92-97045e367fd0>
2010-10-05 12:17:50 +0000

    Actualizadas las cabeceras y la licencia GPL v3

--- trunk/ProyectoIG/_ply.h
+++ trunk/ProyectoIG/_ply.h
@@ -12,8 +12,21 @@
  * Implementation of .ply for read objects in 3D and build
  * @author Miguel Ángel Díaz López
  *
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
-
 #ifndef _PLY_H
 #define _PLY_H
 
